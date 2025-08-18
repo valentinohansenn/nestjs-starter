e1a4dc0d49c5b464a59707e6ac1ec9baad390efc
Valentino Hansen <vhan0350@uni.sydney.edu.au>
2025-08-18 14:18:30 +1000

    feat: implement a simple logger middleware after finishing the middleware section

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './cats/middleware/logger.middleware';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST },
+        'cats/{*splat}',
+      )
+      .forRoutes(CatsController);
+    //   Route wildcards (splat is simply the name of the wildcard parameters)
+    //   .forRoutes({ path: "abcd/*splat", method: RequestMethod.ALL })
+  }
+}
